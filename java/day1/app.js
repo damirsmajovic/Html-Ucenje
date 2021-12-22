139b53100c7466aee1c66aec6e9392af79e7341b
Damir Smajovic <94151173+Dado701@users.noreply.github.com>
2021-12-22 20:20:58 +0100

    first commit

---
+++ java/day1/app.js
@@ -0,0 +1,9 @@
+/*ime = "Damir Smajovic";
+godine = 20;
+console.log("Moje ime je:",ime, "i imam :",godine, "godina"); */
+/*ime = prompt("Unesite vase ime i prezime");
+godine = prompt("Uniestie broj godina");
+console.log("Moje ime je:",ime, "i imam :",godine, "godina");*/
+broj1 = prompt("Unesite prvi broj");
+broj2 = prompt("Unesite drugi broj");
+console.log("Zbir prvog",broj1,"i drugo broja",broj2,"jednak je:",parseInt(broj1)+parseInt(broj2));
---
+++ java/day2/app.js
@@ -0,0 +1,44 @@
+/*console.log(8>5);
+console.log(5=="5");
+console.log(5==="5");
+console.log("daris"==="daris");
+console.log("daris"==="Daris");
+console.log(Boolean(52));
+console.log(Boolean(0));
+console.log(Boolean(""));
+console.log(Boolean(" "));
+var1=true;
+var2=false;
+console.log(true+true);*/
+/*false || false === false;
+true || false === true;
+true || true === true;
+false || true === true;
+
+false && false === false;
+false && true === false;
+true && false === false;
+true && true === true;*/
+
+/*a=5;
+b=10;
+console.log(a>b);
+console.log(a<b);*/
+
+/*a=prompt("Unesite prvi broj");
+b=prompt("Unesite drugi broj");
+console.log("Da li je a vece od b:",parseInt(a)>parseInt(b));*/
+/*
+a=prompt("Unesite broj koji je veci od 1 manji od 10 ")
+console.log("Broj a je:",parseInt(a)<=10 && parseInt(a)>=1);*/
+/*
+a=prompt("Unesite broj")
+console.log("Broj je paran",parseInt(a)%2==0);*/
+/*
+a="Damir"
+console.log("Damir".length);
+console.log(a.length);
+*/
+ime=prompt("Unesite vase ime");
+duzina=ime.length;
+console.log("Vase ime ima :",duzina, "slova");
