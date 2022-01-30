65284a58a19cda7d6b8600bdad0a51d052af8195
Damir Smajovic <94151173+Dado701@users.noreply.github.com>
2022-01-30 13:48:58 +0100

    commit

---
+++ domaci/j-domaci13/app.js
@@ -0,0 +1,13 @@
+otac=parseInt(prompt("Unesite godiste oca"))
+sin=parseInt(prompt("Unesite godiste sina"))
+counter=0;
+for(i=0;i<otac;i++){
+ if(otac/sin===2){
+  break
+ }
+otac--
+sin--
+counter++
+}
+console.log(`Otac je bio duplo stariji kad je imao ${otac} , a sin ${sin}`);
+console.log(counter);
---
+++ java/day15/app.js
@@ -0,0 +1,69 @@
+// arr=[[1,2,3],2]
+// console.log(arr[0][1]);
+
+// function sayHello() {
+// console.log("Moje ime je Damir");
+// }
+// sayHello();
+
+// function sum(){
+//  a=parseInt(prompt("Unesite prvi broj"))
+//  b=parseInt(prompt("Unesite drugi broj"))
+//  console.log(a+b);
+// }
+// sum()
+
+// function sayHello(a,b){
+//  console.log(a+b);
+// }
+// sayHello(2,5)
+
+// function mojaF(){
+//  console.log("asd");
+//  console.log("ccc");
+// return "Okay";
+// console.log("asddsada");
+// }
+// a=mojaF()
+// mojaF()
+// console.log(a);
+
+
+// novo="";
+// str="Damir"
+// function solution(str){
+//  for(i=str.length-1;i>=0;i--){
+//   novo+=str[i]
+//  }
+//  console.log(novo);
+//  return novo
+// }
+// solution(str)
+
+// str="Damir"
+// s=""
+// function solution(str){
+//  for(i=0;i<str.length;i++){
+//   s+=str[i].toUpperCase()
+//  }
+//  console.log(s);
+// }
+// solution(str)
+
+// s=""
+// function solution(n,a){
+//  for(i=0;i<n;i++){
+//   console.log(a);
+//  }
+// }
+// solution(2,"Nesto")
+
+// function str(bool){
+//  if(!bool==false){
+//   console.log("Yes");
+//  }
+//  else if(!bool==true){
+//   console.log("No");
+//  }
+// }
+// str(0)
