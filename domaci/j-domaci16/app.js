0edaf9bb0f3325748f7d2fae57fd33609b9cc8da
Damir Smajovic <94151173+Dado701@users.noreply.github.com>
2022-02-07 17:56:37 +0100

    commit

---
+++ domaci/j-domaci16/app.js
@@ -0,0 +1,21 @@
+// a="Danas je mnogo lep dan"
+// function findShort(s){
+//   s=s.split(" ")
+//   min=s[0];
+//   for(i=0;i<s.length;i++){
+//     if(s[i].length<min.length){
+//       min=s[i]
+//     }
+//   }console.log(min.length);
+// }
+// findShort(a)
+a=["a",5,2,"nesto",3,1]
+function filter_list(l) {
+  s=[]
+  for(i=0;i<l.length;i++){
+    if(typeof l[i] !== 'string' ){
+      s.push(l[i])
+    }
+  }console.log(s); 
+}
+filter_list(a)
