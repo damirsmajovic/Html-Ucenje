b45e7f0e78a65d4bd071fbe0db97d4c4890d7965
Damir Smajovic <94151173+Dado701@users.noreply.github.com>
2022-02-18 19:30:51 +0100

    commit

--- java/day21/app.js
+++ java/day21/app.js
@@ -7,17 +7,29 @@
 // }
 // arr.forEach(a)
 // console.log(novi);
-s="DaNas je neSto lepo"
-novi=[]
+
+
+// s="DaNas je neSto lepo"
+// novi=[]
+// function a(a){
+//  for(i=0;i<a.length;i++){
+//   if(i%2==0 && a[i]!==" "){
+//   novi.push(a[i].toLowerCase())
+//  }
+//  else if(a[i]!==" "){
+//   novi.push(a[i].toUpperCase());
+//  }
+//  }
+//  console.log(novi.join(""));
+// }
+// a(s)
+
+const data=[NaN,0,15,false,-22,"",undefined,47,null]
+br=0
 function a(a){
- for(i=0;i<a.length;i++){
-  if(i%2==0 && a[i]!==" "){
-  novi.push(a[i].toLowerCase())
+ if(!Boolean(a)){
+  br++
  }
- else if(a[i]!==" "){
-  novi.push(a[i].toUpperCase());
- }
- }
- console.log(novi.join(""));
 }
-a(s)
+data.forEach(a)
+console.log(br);
