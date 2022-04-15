38df94953539db9651576c4c83092d2ec3a22ea4
Damir Smajovic <94151173+Dado701@users.noreply.github.com>
2022-04-16 01:01:20 +0200

    commit

---
+++ practice/app.js
@@ -0,0 +1,38 @@
+// n="Revelation of the contents outraged American public opinion, and helped generate"
+// function dropCap(n) {
+// a=n.split(" ")
+// s=""
+//   for(i=0;i<a.length;i++){
+// if(a[i].length>2){
+//     s+=a[i][0].toUpperCase()+a[i].slice(1,a[i].length)+" "
+// }    
+// else{
+//   s+=a[i]+" "
+// }
+    
+//   }
+//   console.log(s);
+// }
+// dropCap(n)
+
+// n=[12,34,56,78]
+// function unusedDigits(n) {
+//   n=n.join("")
+//   nema=""
+//   for(i=0;i<10;i++){
+//     ima=0;
+//       for(j=0;j<n.length;j++){
+//         if(n[j]==i){
+//           ima++
+//         }
+//       }
+//       if(ima==0){
+//         nema+=i
+//       }
+//   }
+
+//   console.log(nema);
+// }
+// unusedDigits(n)
+// n=n.join("")
+// console.log(n.length);
