a0680256f178ff7be1c867e2686b44a9c0d4864b
Damir Smajovic <94151173+Dado701@users.noreply.github.com>
2022-04-17 00:58:27 +0200

    commit

--- practice/app.js
+++ practice/app.js
@@ -36,3 +36,47 @@
 // unusedDigits(n)
 // n=n.join("")
 // console.log(n.length);
+
+function isOddHeavy(n){
+  max=n[0];
+  odd=0
+  nema=0
+  veci=0
+  el=[]
+  if(n.length==0){
+    return false
+  }
+  for(i=0;i<n.length;i++){
+    if(n[i]%2!==0 && n[i]>max){
+      max=n[i]
+    }
+  }
+  for(i=0;i<n.length;i++){
+    if(n[i]>max){
+      odd++
+    }
+    if(n[i]%2!==0){
+      nema++
+      el.push(n[i])
+    }
+  }
+  for(i=0;i<el.length;i++){
+    for(j=0;j<n.length;j++){
+      if(el[i]<n[j]){
+        veci++
+      }
+    }
+  }
+  if(veci>0){
+    return false
+  }
+  if(nema==0){
+    return false
+  }
+  if(odd>0){
+    return false
+  }
+  else{
+    return true
+  }
+}
