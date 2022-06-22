8acc28c3a4ca0ac904a965a3ce478cfa40af28eb
Damir Smajovic <94151173+Dado701@users.noreply.github.com>
2022-06-22 15:55:35 +0200

    commit

--- practice/app.js
+++ practice/app.js
@@ -37,46 +37,71 @@
 // n=n.join("")
 // console.log(n.length);
 
-function isOddHeavy(n){
-  max=n[0];
-  odd=0
-  nema=0
-  veci=0
-  el=[]
-  if(n.length==0){
-    return false
+// function isOddHeavy(n){
+//   max=n[0];
+//   odd=0
+//   nema=0
+//   veci=0
+//   el=[]
+//   if(n.length==0){
+//     return false
+//   }
+//   for(i=0;i<n.length;i++){
+//     if(n[i]%2!==0 && n[i]>max){
+//       max=n[i]
+//     }
+//   }
+//   for(i=0;i<n.length;i++){
+//     if(n[i]>max){
+//       odd++
+//     }
+//     if(n[i]%2!==0){
+//       nema++
+//       el.push(n[i])
+//     }
+//   }
+//   for(i=0;i<el.length;i++){
+//     for(j=0;j<n.length;j++){
+//       if(el[i]<n[j]){
+//         veci++
+//       }
+//     }
+//   }
+//   if(veci>0){
+//     return false
+//   }
+//   if(nema==0){
+//     return false
+//   }
+//   if(odd>0){
+//     return false
+//   }
+//   else{
+//     return true
+//   }
+// }
+
+// Multiples of 3 or 5
+numbers=10
+function solution(number){
+  n=[]
+  s=0
+  if(number<0){
+    return 0
   }
-  for(i=0;i<n.length;i++){
-    if(n[i]%2!==0 && n[i]>max){
-      max=n[i]
+  for(i=1;i<=number-1;i++){
+    if(i%3==0 && i%5==0){
+      n.push(i)
+    }
+    else if(i%3==0){
+      n.push(i)
+    }
+    else if(i%5==0){
+      n.push(i)
     }
   }
   for(i=0;i<n.length;i++){
-    if(n[i]>max){
-      odd++
-    }
-    if(n[i]%2!==0){
-      nema++
-      el.push(n[i])
-    }
+    s+=n[i]
   }
-  for(i=0;i<el.length;i++){
-    for(j=0;j<n.length;j++){
-      if(el[i]<n[j]){
-        veci++
-      }
-    }
-  }
-  if(veci>0){
-    return false
-  }
-  if(nema==0){
-    return false
-  }
-  if(odd>0){
-    return false
-  }
-  else{
-    return true
-  }
-}
+console.log(s);}
+solution(numbers)
